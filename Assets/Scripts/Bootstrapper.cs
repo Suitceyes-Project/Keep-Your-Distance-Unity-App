9701ec49b8327ccc6586e84632b0064b8c0129e8
James Gay <g.james@gmx.net>
2019-09-09 13:53:08 +0200

    added Bootstrapper and UI scenes, added ACI Unity Tools

---
+++ Assets/Scripts/Bootstrapper.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Aci.KeepYourDistance
+{
+    public class Bootstrapper : MonoBehaviour
+    {
+        private async void Start()
+        {
+#if !UNITY_EDITOR
+
+            await SceneManager.LoadSceneAsync("scn_UI", LoadSceneMode.Additive);                      
+#endif
+
+            if (!Debug.isDebugBuild)
+                Debug.unityLogger.logEnabled = false;
+
+        }
+    }
+}
