b08a76eede6af920a4c2dfab7a32023e45432e70
James Gay <g.james@gmx.net>
2019-09-09 17:08:10 +0200

    added simple hello world test

---
+++ Assets/Scripts/ViewController/MainMenuViewController.cs
@@ -0,0 +1,23 @@
+﻿using Realtime.Ortc;
+using UnityEngine;
+
+namespace Aci.KeepYourDistance.ViewControllers
+{
+    public class MainMenuViewController : MonoBehaviour
+    {
+        private const string Channel = "ACI_KYD";
+
+        private IOrtcClient m_OrtcClient;
+
+        [Zenject.Inject]
+        private void Construct(IOrtcClient ortcClient)
+        {
+            m_OrtcClient = ortcClient;
+        }
+
+        public void HelloWorld()
+        {
+            m_OrtcClient.Send(Channel, "Hello World");
+        }
+    }
+}
